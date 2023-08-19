d0ff5005efd04d3548d4ff93b5e9e4146dbb53a3
Agryo Araujo <agryostallion@gmail.com>
2023-08-18 21:37:30 -0300

    Criação do componente Card-Label

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-label',
   templateUrl: './card-label.component.html',
   styleUrls: ['./card-label.component.css']
 })
-export class CardLabelComponent {
+export class CardLabelComponent implements OnInit {
+  //O Input e a gameLabel são para deixar o componente dinâmico e virar um parâmetro lá na Tag HTML.
+  @Input()
+  gameLabel:String=""
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
 
 }
