af661086fee2796f5c7a83a54011cdbf8c9ff95c
Agryo Araujo <agryostallion@gmail.com>
2023-08-18 22:14:56 -0300

    Criação do componente Card-Price

--- src/app/components/card/card-price/card-price.component.ts
+++ src/app/components/card/card-price/card-price.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-price',
   templateUrl: './card-price.component.html',
   styleUrls: ['./card-price.component.css']
 })
-export class CardPriceComponent {
+export class CardPriceComponent implements OnInit {
+  @Input()
+  gameTipo:string = "Digital PS5"
+  @Input()
+  gamePreco:string = "R$ 348,98"
+  
+  constructor() { }
 
+  ngOnInit(): void {
+    
+  }
 }
